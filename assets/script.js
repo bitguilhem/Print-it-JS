4205c167a73671461f58e9f2a2afc55b8b2d299e
guilhem durbal <gdurbal@gmail.com>
2023-12-22 06:09:53 +0100

    derniere etape

--- assets/script.js
+++ assets/script.js
@@ -47,3 +47,63 @@ arrowRight.addEventListener('mousedown', handleArrowClick);
 
 const dotselected = document.querySelector('.dot_selected');
 console.log("The little hot dog")
+
+
+// JavaScript
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const dots = document.querySelectorAll('.dot');
+const slideText = document.getElementById('slideText');
+const bannerImages = [
+    './assets/images/slideshow/slide1.jpg',
+    './assets/images/slideshow/slide2.jpg',
+    './assets/images/slideshow/slide3.jpg',
+    './assets/images/slideshow/slide4.jpg'
+];
+let currentSlideIndex = 0;
+
+function setActiveDot(index) {
+    dots.forEach(dot => dot.classList.remove('dot_selected'));
+    dots[index].classList.add('dot_selected');
+}
+
+function changeSlide(direction) {
+    if (direction === 'right') {
+        currentSlideIndex = (currentSlideIndex + 1) % bannerImages.length;
+    } else if (direction === 'left') {
+        currentSlideIndex = (currentSlideIndex - 1 + bannerImages.length) % bannerImages.length;
+    }
+
+    // Mettre à jour le point actif
+    setActiveDot(currentSlideIndex);
+
+    // Mettre à jour l'image
+    document.querySelector('.banner-img').src = bannerImages[currentSlideIndex];
+
+    // Mettre à jour le texte
+    switch (currentSlideIndex) {
+        case 0:
+            slideText.textContent = 'Impressions tous formats en boutique et en ligne';
+            break;
+        case 1:
+            slideText.textContent = 'Un autre texte pour la deuxième image';
+            break;
+        case 2:
+            slideText.textContent = 'Encore un autre texte pour la troisième image';
+            break;
+        case 3:
+            slideText.textContent = 'Et enfin, un texte pour la quatrième image';
+            break;
+        // Ajoutez plus de cas si nécessaire
+    }
+}
+
+// Écouteur d'événements pour la flèche droite
+arrowRight.addEventListener('click', function() {
+    changeSlide('right');
+});
+
+// Écouteur d'événements pour la flèche gauche
+arrowLeft.addEventListener('click', function() {
+    changeSlide('left');
+});
